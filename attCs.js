b92c057a819587b191b237c08882f00c7b56e419
js-all <titouangoulois29@gmail.com>
2018-09-28 19:58:25 +0200

    fg

---
+++ attCs.js
@@ -0,0 +1,10 @@
+var style = document.createElement('style');
+// code ...
+var els = document.querySelectorAll('e');
+for (let i = 0;i < document.all.length;i++) {
+    if (document.all[i].hasAttribute('e')) {
+        alert(document.all[i]);
+    }
+}
+// code ...
+document.head.appendChild(style)
